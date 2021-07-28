cb29d8bb1fa40cc2bfaf9bc5c27377264b5fe2c9
Daniel Zarifpour <dzarifpour@gmail.com>
2021-07-28 14:59:49 -0400

    Added anchors for fullpage.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactFullpage from '@fullpage/react-fullpage';
 import styles from '../styles/Home.module.css'
-// import { Cards } from '../components/Cards'
 import Card from '../components/Card'
 import { Skills } from '../components/Skills'
 import { Scroll } from '../components/Scroll'
@@ -13,40 +12,46 @@ const Home = () => (
         //FullPage options
         licenseKey={'null'}
         scrollingSpeed={800} /* Options here */
+        lockAnchors={true}
+        anchors={['p1', 'p2', 'p3']}
+        // scrollBar={true}
+        // verticalAlign={true}
 
         render={({ state, fullpageApi }) => {
             return (
-                <ReactFullpage.Wrapper>
-                    {/* <div className="section">
+                <>
+                    <ReactFullpage.Wrapper>
+                        {/* <div className="section">
                         <div class="flex">
                             <FunctionClick />
                         </div>
                     </div> */}
-                    <div className="section">
-                        <Scroll />
-                        <div className="landing flex">
-                            <div className="title-box no-pointer flex">
-                                <div className={`${styles['ls-bug']} gradient ${styles['zarif']} fade-in`}>Daniel Zarifpour</div>
-                                <div className="break"></div>
-                                <div className={`${styles['zarif-2']} fade-in anim-delay-2s`}>
-                                    <div className={`${styles['ls-bug']} gradient`}>Creator</div>
-                                    <div className={`${styles['ls-bug']} ${styles['lh-bug']} gradient`} style={{ margin: "0px 12px" }}>|</div>
-                                    <div className={`${styles['ls-bug']} gradient`}>Engineer</div>
+                        <div className="section">
+                            <Scroll />
+                            <div className="landing flex">
+                                <div className="title-box no-pointer flex">
+                                    <div className={`${styles['ls-bug']} gradient ${styles['zarif']} fade-in`}>Daniel Zarifpour</div>
+                                    <div className="break"></div>
+                                    <div className={`${styles['zarif-2']} fade-in anim-delay-2s`}>
+                                        <div className={`${styles['ls-bug']} gradient`}>Creator</div>
+                                        <div className={`${styles['ls-bug']} ${styles['lh-bug']} gradient`} style={{ margin: "0px 12px" }}>|</div>
+                                        <div className={`${styles['ls-bug']} gradient`}>Engineer</div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="section">
-                        <Card
-                            title="DApps"
-                            description="A collection of decentralized applications, including a faucet, an advertisement auction, and a voting app."
-                            link="/dapps"    
-                        />
-                    </div>
-                    <div className="section">
-                        <Skills />
-                    </div>
-                </ReactFullpage.Wrapper>
+                        <div className="section">
+                            <Card
+                                title="DApps"
+                                description="A collection of decentralized applications, including a faucet, an advertisement auction, and a voting app."
+                                link="/dapps"
+                            />
+                        </div>
+                        <div className="section">
+                            <Skills />
+                        </div>
+                    </ReactFullpage.Wrapper>
+                </>
             );
         }}
     />
