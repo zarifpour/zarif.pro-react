73595591e4ce0608fabe349395a43f8b714b7336
Daniel Zarifpour <dzarifpour@gmail.com>
2021-07-29 04:23:08 -0400

    Commented unused functions

--- components/Stars.js
+++ components/Stars.js
@@ -5,40 +5,42 @@ import { OrbitControls, Stars as Space } from "@react-three/drei";
 import star from "../public/img/star.png"
 import starsStyles from '../styles/Stars.module.css'
 
-function useWindowSize() {
-    // Initialize state with undefined width/height so server and client renders match
-    // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
-    const [windowSize, setWindowSize] = useState({
-        width: undefined,
-        height: undefined,
-    });
+// function useWindowSize() {
+//     // Initialize state with undefined width/height so server and client renders match
+//     // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+//     const [windowSize, setWindowSize] = useState({
+//         width: undefined,
+//         height: undefined,
+//     });
 
-    useEffect(() => {
-        function handleResize() {
-            // Set window width/height to state
-            setWindowSize({
-                width: window.innerWidth,
-                height: window.innerHeight,
-            });
-        }
+//     useEffect(() => {
+//         function handleResize() {
+//             // Set window width/height to state
+//             setWindowSize({
+//                 width: window.innerWidth,
+//                 height: window.innerHeight,
+//             });
+//         }
 
-        // Add event listener
-        window.addEventListener("resize", handleResize);
+//         // Add event listener
+//         window.addEventListener("resize", handleResize);
 
-        // Call handler right away so state gets updated with initial window size
-        handleResize();
+//         // Call handler right away so state gets updated with initial window size
+//         handleResize();
 
-        // Remove event listener on cleanup
-        return () => window.removeEventListener("resize", handleResize);
-    }, []); // Empty array ensures that effect is only run on mount
-    // console.log(windowSize);
-    return windowSize;
-}
+//         // Remove event listener on cleanup
+//         return () => window.removeEventListener("resize", handleResize);
+//     }, []); // Empty array ensures that effect is only run on mount
+//     // console.log(windowSize);
+//     return windowSize;
+// }
 
 export const Stars = () => {
-    const size = useWindowSize();
-    const width = size.width;
-    const height = size.height;
+    // const size = useWindowSize();
+    // const width = size.width;
+    // const height = size.height;
+
+
 
     // let scene, camera, renderer, stars, starGeo;
     // function init() {
@@ -109,6 +111,8 @@ export const Stars = () => {
     //     requestAnimationFrame(animate);
     // }
     // init();
+
+
 
     return (
         <Canvas id={starsStyles['star-canvas']} className="fade-in">
